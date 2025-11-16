761e613a66ce5c9f7900f121f8cf4200929c1b10
AnvPlayzz@1 <av95766@gmail.com>
2025-11-16 14:52:54 +0530

    refactor: standardize quotation marks in booking.js

--- models/tour/booking.js
+++ models/tour/booking.js
@@ -1,47 +1,53 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const tourBookingSchema = new mongoose.Schema(
-    {
-        bookingId: {
-            type: String,
-            unique: true,
-            default: () =>
-                [...Array(10)]
-                    .map(() => {
-                        const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-                        return chars.charAt(Math.floor(Math.random() * chars.length));
-                    })
-                    .join(''),
-        },
-        userId: { type: String, required: true },
-        travelId: { type: String, required: true },
-        visitngPlaces: { type: String },
-        travelAgencyName: { type: String },
-        country: { type: String },
-        state: { type: String },
-        city: { type: String },
-        themes: { type: String },
-        price: { type: Number },
-        nights: { type: Number },
-        days: { type: Number },
-        from: { type: Date },
-        to: { type: Date },
-        amenities: [String],
-        inclusion: [String],
-        exclusion: [String],
-        termsAndConditions: {
-            type: Map,
-            of: String,
-        },
-        dayWise: [
-            {
-                day: { type: Number },
-                description: { type: String },
-            },
-        ],
+  {
+    bookingId: {
+      type: String,
+      unique: true,
+      default: () =>
+        [...Array(10)]
+          .map(() => {
+            const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return chars.charAt(Math.floor(Math.random() * chars.length));
+          })
+          .join(""),
     },
-    { timestamps: true, strict: false }
+    userId: { type: String, required: true },
+    travelId: { type: String, required: true },
+    visitngPlaces: { type: String },
+    travelAgencyName: { type: String },
+    country: { type: String },
+    customizable: { type: Boolean, default: false },
+    numberOfAdults: { type: Number },
+    numberOfChildren: { type: Number },
+    childDateOfBirth: [Date],
+    childPrices: [Number],
+    tourStartDate: { type: Date },
+    state: { type: String },
+    city: { type: String },
+    themes: { type: String },
+    price: { type: Number },
+    nights: { type: Number },
+    days: { type: Number },
+    from: { type: Date },
+    to: { type: Date },
+    amenities: [String],
+    inclusion: [String],
+    exclusion: [String],
+    termsAndConditions: {
+      type: Map,
+      of: String,
+    },
+    dayWise: [
+      {
+        day: { type: Number },
+        description: { type: String },
+      },
+    ],
+  },
+  { timestamps: true, strict: false }
 );
 
-const TourBooking = mongoose.model('TourBooking', tourBookingSchema);
+const TourBooking = mongoose.model("TourBooking", tourBookingSchema);
 module.exports = TourBooking;
